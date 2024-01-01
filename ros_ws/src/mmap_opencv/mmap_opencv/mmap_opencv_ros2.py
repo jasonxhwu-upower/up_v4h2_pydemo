03eff82be9ec6f6919c997ac99b7ccccfafa7fb7
Jason Wu <jason.wu@upower.com>
2024-01-01 08:12:25 -0800

    refactored code again

--- ros_ws/src/mmap_opencv/mmap_opencv/mmap_opencv_ros2.py
+++ ros_ws/src/mmap_opencv/mmap_opencv/mmap_opencv_ros2.py
@@ -1,19 +1,14 @@
 import rclpy
 from rclpy.node import Node
-
 from std_msgs.msg import String
-
-import cv2
-import numpy as np
-import mmap
-
+from utils import mmap_v4h
 
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
         self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
+        timer_period = 0.001  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
@@ -21,38 +16,9 @@ class MinimalPublisher(Node):
         msg = String()
         msg.data = 'Hello World: %d' % self.i
         self.publisher_.publish(msg)
-        # self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
-        self.mmap_main() # uncomment this line to run mmap_opencv function
-
-    
-    def read_yuyv_image_to_8bit(self, width, height):
-        with open("image_buffer_out.dat", "r+b") as f:
-            mmapped_file = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-        # Read and print the data
-        data = mmapped_file.read()
-        numpy_array = np.frombuffer(data, dtype=np.uint8)
-        return numpy_array.reshape((height, width, 2))
-
-    def convert_yuv_to_bgr(self, yuv_image):
-        bgr_image = cv2.cvtColor(yuv_image, cv2.COLOR_YUV2BGR_YUYV)
-        return bgr_image
-    
-    def mmap_main(self):
-        file_path = 'test.raw'
-        output_path = 'output.bmp'
-        jpeg_filename = "output.jpg"
-        width = 1280 
-        height = 720
-        while True:
-            uyvy_values = self.read_yuyv_image_to_8bit(width,height)
-            rgb_image = self.convert_yuv_to_bgr(uyvy_values)
-            #cv2.imwrite(jpeg_filename, rgb_image)
-            cv2.imshow("Image", rgb_image)
-            cv2.waitKey(1)
-
-        cv2.destroyAllWindows()
-
+        #self.mmap_main() # uncomment this line to run mmap_opencv function
 
 def main(args=None):
     rclpy.init(args=args)
