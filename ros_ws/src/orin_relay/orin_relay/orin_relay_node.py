418da11f900fd5d424e825d9b0aed0217eb3a8e2
Jason Wu <jason.wu@upower.com>
2024-01-01 20:05:14 -0800

    cleaned up orin_relay node up, still barebones

--- ros_ws/src/orin_relay/orin_relay/orin_relay_node.py
+++ ros_ws/src/orin_relay/orin_relay/orin_relay_node.py
@@ -1,6 +1,31 @@
-def main():
-    print('Hi from orin_relay.')
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile, HistoryPolicy, DurabilityPolicy, ReliabilityPolicy
+from sensor_msgs.msg import CompressedImage
+#import v4h_relay.utils.mmap_v4h as mmap_utils
 
+class orin_relay(Node):
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        qos_profile = QoSProfile(
+            depth=1,
+            history=HistoryPolicy.KEEP_LAST,
+            reliability=ReliabilityPolicy.BEST_EFFORT,
+            durability=DurabilityPolicy.VOLATILE
+        )
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    orin_main = orin_relay()
+
+    rclpy.spin(orin_main)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    orin_main.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
