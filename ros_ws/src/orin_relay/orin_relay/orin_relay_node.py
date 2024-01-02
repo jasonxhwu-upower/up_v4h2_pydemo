6a1fde2339125d30675b29fc392cdca64ba4ddde
Lawrence Xu <lawrence.xu@upower.com>
2024-01-02 21:30:14 +0800

    bidirectional publisher from orin

--- ros_ws/src/orin_relay/orin_relay/orin_relay_node.py
+++ ros_ws/src/orin_relay/orin_relay/orin_relay_node.py
@@ -15,11 +15,16 @@ class orin_relay(Node):
         )
         #subscriber
         self.subscription = self.create_subscription(CompressedImage, '/v4h_topic', self.listener_callback, qos_profile)
+        self.publisher_ = self.create_publisher(CompressedImage, '/inference_results', qos_profile) # TODO: change CompressedImage to inference results type
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
         test_data = msg.data[:15]
         self.get_logger().info('The image is: "%s"...' % test_data)
+        # ###########################
+        # do some inference work here
+        # ###########################
+        self.publisher_.publish(msg) # TODO: change test_data to inference results
 
 def main(args=None):
     rclpy.init(args=args)
