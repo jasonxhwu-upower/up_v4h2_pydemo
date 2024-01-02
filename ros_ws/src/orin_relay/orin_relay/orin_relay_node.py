31b3c02f6e8b97c0bc464c4b91f7294fbd523fe2
Jason Wu <jason.wu@upower.com>
2024-01-02 15:40:43 -0800

    incorporated util code to main

--- ros_ws/src/orin_relay/orin_relay/orin_relay_node.py
+++ ros_ws/src/orin_relay/orin_relay/orin_relay_node.py
@@ -2,28 +2,27 @@ import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, HistoryPolicy, DurabilityPolicy, ReliabilityPolicy
 from sensor_msgs.msg import CompressedImage
-#import v4h_relay.utils.mmap_v4h as mmap_utils
+import orin_relay.utils.mmap_orin as mmap_utils
 
 class orin_relay_sub(Node):
     def __init__(self):
         super().__init__('minimal_subscriber')
-
         qos_profile = QoSProfile(
             depth=1,
             history=HistoryPolicy.KEEP_LAST,
             reliability=ReliabilityPolicy.BEST_EFFORT,
             durability=DurabilityPolicy.VOLATILE
         )
-        
         #subscriber
         self.subscription = self.create_subscription(CompressedImage, '/v4h2_to_orin', self.listener_callback, qos_profile)
-        self.publisher_ = self.create_publisher(CompressedImage, '/inference_results', qos_profile) # TODO: change CompressedImage to inference results type
-        self.subscription  # prevent unused variable warning
+        mmap_utils.open_from_v4h2_mmap()
 
     def listener_callback(self, msg):
-        test_data = msg.data[:15]
-        self.get_logger().info('The image is: "%s"...' % test_data)
-        self.publisher_.publish(msg) # TODO: change test_data to inference results
+        msg_data = mmap_utils.orin_sub_mmap(msg)
+
+    def destroy_node(self):
+        mmap_utils.close_from_v4h2_mmap()
+        super().destroy_node()
 
 class orin_relay_pub(Node):
     def __init__(self):
@@ -41,6 +40,8 @@ class orin_relay_pub(Node):
         self.fps_counter = self.create_timer(1, self.count_fps)
         #timer callback counter
         self.i = 0
+        #mmap open
+        mmap_utils.open_orin_inference_mmap()
     
     def timer_callback(self):
         msg = CompressedImage()
@@ -53,13 +54,17 @@ class orin_relay_pub(Node):
         self.get_logger().info('Number of frames published per second: %d' % self.i)
         self.i = 0
 
+    def destroy_node(self):
+        mmap_utils.close_orin_inference_mmap()
+        super().destroy_node()
+
 def main(args=None):
     rclpy.init(args=args)
     executor = rclpy.executors.MultiThreadedExecutor()
-    orin_pub = orin_relay_pub()
+    #orin_pub = orin_relay_pub()
     orin_sub = orin_relay_sub()
 
-    executor.add_node(orin_pub)
+    #executor.add_node(orin_pub)
     executor.add_node(orin_sub)
 
     try:
@@ -68,8 +73,8 @@ def main(args=None):
     except:
         pass
     
+    #orin_pub.destroy_node()
     orin_sub.destroy_node()
-    orin_pub.destroy_node()
     executor.shutdown()
     rclpy.shutdown()
 
