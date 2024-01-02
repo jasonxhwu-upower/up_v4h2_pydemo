0b65f06c945d1d3a00d72854a3a06f7204709a48
Jason Wu <jason.wu@upower.com>
2024-01-02 12:44:31 -0800

    Changed into dual-channel relays

--- ros_ws/src/orin_relay/orin_relay/orin_relay_node.py
+++ ros_ws/src/orin_relay/orin_relay/orin_relay_node.py
@@ -4,39 +4,73 @@ from rclpy.qos import QoSProfile, HistoryPolicy, DurabilityPolicy, ReliabilityPo
 from sensor_msgs.msg import CompressedImage
 #import v4h_relay.utils.mmap_v4h as mmap_utils
 
-class orin_relay(Node):
+class orin_relay_sub(Node):
     def __init__(self):
         super().__init__('minimal_subscriber')
+
         qos_profile = QoSProfile(
             depth=1,
             history=HistoryPolicy.KEEP_LAST,
             reliability=ReliabilityPolicy.BEST_EFFORT,
             durability=DurabilityPolicy.VOLATILE
         )
+        
         #subscriber
-        self.subscription = self.create_subscription(CompressedImage, '/v4h_topic', self.listener_callback, qos_profile)
+        self.subscription = self.create_subscription(CompressedImage, '/v4h2_to_orin', self.listener_callback, qos_profile)
         self.publisher_ = self.create_publisher(CompressedImage, '/inference_results', qos_profile) # TODO: change CompressedImage to inference results type
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
         test_data = msg.data[:15]
         self.get_logger().info('The image is: "%s"...' % test_data)
-        # ###########################
-        # do some inference work here
-        # ###########################
         self.publisher_.publish(msg) # TODO: change test_data to inference results
+
+class orin_relay_pub(Node):
+    def __init__(self):
+        super().__init__('minimal_publisher')
+
+        qos_profile = QoSProfile(
+            depth=1,
+            history=HistoryPolicy.KEEP_LAST,
+            reliability=ReliabilityPolicy.BEST_EFFORT,
+            durability=DurabilityPolicy.VOLATILE
+        )
+        #publisher
+        self.publisher_ = self.create_publisher(CompressedImage, '/orin_to_v4h2', qos_profile)
+        self.timer = self.create_timer(0, self.timer_callback)
+        self.fps_counter = self.create_timer(1, self.count_fps)
+        #timer callback counter
+        self.i = 0
+    
+    def timer_callback(self):
+        msg = CompressedImage()
+        msg.format = 'jpeg'
+        #msg.data = mmap_utils.read_frontcam_membuf().tobytes()
+        #self.publisher_.publish(msg)
+        self.i += 1
+        
+    def count_fps(self):
+        self.get_logger().info('Number of frames published per second: %d' % self.i)
+        self.i = 0
 
 def main(args=None):
     rclpy.init(args=args)
+    executor = rclpy.executors.MultiThreadedExecutor()
+    orin_pub = orin_relay_pub()
+    orin_sub = orin_relay_sub()
 
-    orin_main = orin_relay()
+    executor.add_node(orin_pub)
+    executor.add_node(orin_sub)
 
-    rclpy.spin(orin_main)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    orin_main.destroy_node()
+    try:
+        while rclpy.ok():
+            executor.spin_once()
+    except:
+        pass
+    
+    orin_sub.destroy_node()
+    orin_pub.destroy_node()
+    executor.shutdown()
     rclpy.shutdown()
 
 if __name__ == '__main__':
--- ros_ws/src/v4h_relay/v4h_relay/v4h_relay_ros2.py
+++ ros_ws/src/v4h_relay/v4h_relay/v4h_relay_ros2.py
@@ -3,8 +3,8 @@ from rclpy.node import Node
 from rclpy.qos import QoSProfile, HistoryPolicy, DurabilityPolicy, ReliabilityPolicy
 from sensor_msgs.msg import CompressedImage
 import v4h_relay.utils.mmap_v4h as mmap_utils
-class v4h2_relay(Node):
 
+class v4h2_relay_pub(Node):
     def __init__(self):
         super().__init__('minimal_publisher')
         qos_profile = QoSProfile(
@@ -14,7 +14,7 @@ class v4h2_relay(Node):
             durability=DurabilityPolicy.VOLATILE
         )
         #publisher
-        self.publisher_ = self.create_publisher(CompressedImage, 'v4h_topic', qos_profile)
+        self.publisher_ = self.create_publisher(CompressedImage, '/v4h2_to_orin', qos_profile)
         self.timer = self.create_timer(0, self.timer_callback)
         self.fps_counter = self.create_timer(1, self.count_fps)
         #timer callback counter
@@ -29,19 +29,43 @@ class v4h2_relay(Node):
         
     def count_fps(self):
         self.get_logger().info('Number of frames published per second: %d' % self.i)
-        self.i = 30
+        self.i = 0
+
+class v4h2_relay_sub(Node):
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+
+        qos_profile = QoSProfile(
+            depth=1,
+            history=HistoryPolicy.KEEP_LAST,
+            reliability=ReliabilityPolicy.BEST_EFFORT,
+            durability=DurabilityPolicy.VOLATILE
+        )
+        
+        self.subscription = self.create_subscription(CompressedImage, '/orin_to_v4h2', self.listener_callback, qos_profile)
+    
+    def listener_callback(self, msg):
+        test_data = msg.data[:15]
+        self.get_logger().info('The image is: "%s"...' % test_data)
 
 def main(args=None):
     rclpy.init(args=args)
+    executor = rclpy.executors.MultiThreadedExecutor()
+    v4h2_pub = v4h2_relay_pub()
+    v4h2_sub = v4h2_relay_sub()
 
-    v4h2_main = v4h2_relay()
+    executor.add_node(v4h2_pub)
+    executor.add_node(v4h2_sub)
 
-    rclpy.spin(v4h2_main)
+    try:
+        while rclpy.ok():
+            executor.spin_once()
+    except KeyboardInterrupt:
+        pass
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    v4h2_main.destroy_node()
+    v4h2_pub.destroy_node()
+    v4h2_sub.destroy_node()
+    executor.shutdown()
     rclpy.shutdown()
 
 
